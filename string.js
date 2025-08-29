85e77f5956b9aaef71d22790c4c58965064a2013
Rai Nadeem Abbas <rainadeemkhral786@gmail.com>
2025-08-29 10:12:50 +0000

    string

---
+++ string.js
@@ -0,0 +1,31 @@
+const name = "Nadeem"
+const age = 22
+
+console.log(name + age);
+console.log(`Hello my name is ${name} and my age is ${age}`);
+
+const bookName = new String('English');
+// console.log(bookName);
+// console.log(bookName.length);
+// console.log(bookName.toUpperCase());
+// console.log(bookName.charAt(3));
+// console.log(bookName.indexOf('i'));
+
+
+const newBook = bookName.substring(0,4)
+console.log(newBook);
+
+const newName = "         Nadia Khan       "
+console.log(newName);
+//trim remove spaces from strat and end 
+console.log(newName.trim());
+
+const url = "http://rainadeem.com/@20nadeemjani"
+console.log(url.replace('@20', '$'));
+
+console.log(newName.split(' '))
+//s
+
+
+
+
