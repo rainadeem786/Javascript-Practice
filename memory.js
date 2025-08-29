f982689e746a3b12d926c9835fac2d26b563e354
Rai Nadeem Abbas <rainadeemkhral786@gmail.com>
2025-08-29 09:27:12 +0000

    memory in js

---
+++ memory.js
@@ -0,0 +1,23 @@
+let myName= "Nadeem"
+let anotherName= "Rai"
+console.log(myName);
+console.log(anotherName);
+
+
+
+let userOne = {
+    email: "rai@gmail.com",
+    id : 5112
+}
+
+let userTwo = userOne
+userTwo.email = "tech@gmail.com"
+
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+//two type of memory in js
+//Stack memory  all permitive datatype
+//Heap memory all non permitive datatype 
+
